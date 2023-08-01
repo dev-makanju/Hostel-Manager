07d9abcac6a9ab0c9f1db94169e47e3d59f16ff8
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-08-01 22:43:59 +0100

    staging

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.unshift({
+        test: /\.(tsx|ts|js|mjs|jsx)$/,
+        use: 'swc-loader',
+        exclude: /node_modules/,
+      });
+    }
+    return config;
+  },
+};
+
 
 module.exports = nextConfig
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+// import { Button } from "antd";
+import { Button } from "antd";
 
 export default function Home() {
   return (
-  <div>
-    Body
-  </div>
+    <div>
+      Body
+      <Button type='primary' >Button From Ant Design</Button>
+
+    </div>
   )
 }
