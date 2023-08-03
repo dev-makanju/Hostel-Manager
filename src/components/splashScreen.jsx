22af14c5c092d66e6165739c1e15141a8952c86a
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-08-03 15:56:49 +0100

    logo added

--- src/components/splashScreen.jsx
+++ src/components/splashScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import Image from 'next/image'
+import { logo } from '@/assets'
 const SplashScreen = () => {
   return (
     <div className=' bg-black h-screen text-white flex items-center justify-center ' >
-      
-      <h1 className=' text-sm ' >HostelManager</h1>
+      <div className=' flex justify-center flex-col items-center gap-4 '>
+
+        <Image src={logo} alt='Hostel_Manager_Logo' className=' h-10 w-10 ' />
+      <h1 className=' text-2xl font-normal ' >HostelManager</h1>
       </div>
+    </div>
   )
 }
 
