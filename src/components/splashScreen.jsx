513313c795afd09e0fcfb97dd2d38ba10587e4ba
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-08-03 13:54:54 +0100

    SplashScreen done

--- src/components/splashScreen.jsx
+++ src/components/splashScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SplashScreen = () => {
   return (
-    <div>SplashScreen</div>
+    <div className=' bg-black h-screen text-white flex items-center justify-center ' >SplashScreen</div>
   )
 }
 
